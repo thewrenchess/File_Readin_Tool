e929b8189c4bdf442e57a64d9ba9b019f069fe60
Yangyang Herrera <yherrera@e1z2r5p30.42.us.org>
2016-11-22 14:08:30 -0800

    active read in functions uploaded, extensive debugging needed

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ A tool for file read in w/ active malloc
 
 Created by yherrera.
 
-There is no main in this repository. Download all the .c and .h and call on to ft_filereadin to read in the files.
+There is no main in this repository.
+
+1) Download all the .c and .h
+
+2) Make neccessary changes to fit your program
+
+3) call on
+void	ft_read_in(const char *src, t_list *lst)
+to read in
 
 ^__^
---
+++ readin.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readin.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yherrera <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/22 14:06:53 by yherrera          #+#    #+#             */
+/*   Updated: 2016/11/22 14:07:00 by yherrera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef READIN_H
+# define READIN_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdlib.h>
+
+typedef	struct	s_list
+{
+	size_t		buf_size;
+	char		*str;
+}				t_list;
+
+void			ft_bzero(void *s, size_t n);
+void			*ft_memalloc(size_t size);
+char			*ft_strnew(size_t size);
+char			*ft_strcpy(char *dst, const char *src);
+void			append_list(t_list **lst, char c, size_t i);
+
+#endif
